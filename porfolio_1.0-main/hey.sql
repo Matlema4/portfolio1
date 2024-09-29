64b6a184dc03a7be00e8c018c39ced0141469ba6
Matlema4 <167225263+Matlema4@users.noreply.github.com>
2024-09-30 00:43:40 +0200

    Add files via upload

---
+++ porfolio_1.0-main/hey.sql
@@ -0,0 +1,7 @@
+CREATE TABLE contact_details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO contact_details (phone, email) VALUES ('+27 72 842 6466', 'rakgwalematlema5@gmail.com');
